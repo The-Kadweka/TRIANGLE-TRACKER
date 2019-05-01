8afed15f1facae9e7956cdd9860eb25437ecc779
hussein18149 <husseinkatana17@gmail.com>
2019-05-01 17:43:11 +0300

    did changes on the if and else if statement

--- script.js
+++ script.js
@@ -9,26 +9,24 @@ function myFunction(){
       else if(length1 + length2<=length3 || length1 + length3<=length2 || length2 + length3<=length1){
           document.getElementById("come").innerText="NOT A TRIANGLE!";
       }
-      else if(length1 !== length2 || length1 !== length3 || length3 !== length2) { 
-    
-          document.getElementById("come").innerText="SCALENE TRIANGLE!-none of the sides is equal";
-       }
        else if(length1 === length2 && length2===length3 && length1===length3){
            document.getElementById("come").innerText="EQUILATERAL TRIANGLE!-all sides are equal";
        }
-       else if(length1 === length2 || length2===length3 || length1===length3){
+       else if(length1 == length2 && length1 !==length3 || length2 == length3 && length1 !==length3 || length1==length3 && length1 !==length2){
         document.getElementById("come").innerText="ISOSCELES TRIANGLE!-two sides are equal!"
     }
-
-
-    function myAnswer(){
-        var ANSWER=parseInt(document.getElementById('answer').value);
-
-        if(ANSWER===24){
-            document.getElementById("good").innerHTML="WELL DONE!";
-        }
-        else if(ANSWER >24 || ANSWER<24){
-            document.getElementById("good").innerHTML="KEEP TRYING!";
-        }
+    else if (length1 !==length2 !==length3){
+        document.getElementById("come").innerText="SCALENE TRIANGLE!.all sides are not equal!"
     }
-}
+};
+
+function myAnswer(){
+    var ANSWER=parseInt(document.getElementById('answer').value);
+
+    if(ANSWER===24){
+        document.getElementById("good").innerHTML="WELL DONE!";
+    }
+    else if(ANSWER >24 || ANSWER<24){
+        document.getElementById("good").innerHTML="KEEP TRYING!";
+    }
+};
